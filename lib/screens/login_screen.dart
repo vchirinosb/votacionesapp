d4a1692a436831d31aa5d5cfaa42834170240cd4
Victor Chirinos <vchirinosb@gmail.com>
2024-09-21 13:24:53 -0500

    update content

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:votacionesapp/screens/home_screen.dart';
 import 'package:votacionesapp/services/auth_service.dart';
-import 'home_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
